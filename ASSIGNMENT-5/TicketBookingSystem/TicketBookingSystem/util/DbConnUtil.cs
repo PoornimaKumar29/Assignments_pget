ff6f62e7dc214f27270c75bbd7bf90f40a667b11
PoornimaKumar29 <106436734+PoornimaKumar29@users.noreply.github.com>
2025-04-23 23:23:34 +0530

    Oops implementation

---
+++ ASSIGNMENT-5/TicketBookingSystem/TicketBookingSystem/util/DbConnUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace TicketBookingSystem.util
+{
+    public class DatabaseConnection
+    {
+        private static string connectionString = "Server=POORNIMA\\SQLSERVER2022;Database=TicketBookingSystem;Trusted_Connection=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
